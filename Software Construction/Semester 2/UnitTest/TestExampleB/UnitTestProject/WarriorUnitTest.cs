f3770fd6763325d5f7cc6969c1472009a5553251
Jesper Højlund <github@hillgrove.dk>
2024-03-05 23:54:23 +0100

    Solved UnitTest.2

--- Software Construction/Semester 2/UnitTest/TestExampleB/UnitTestProject/WarriorUnitTest.cs
+++ Software Construction/Semester 2/UnitTest/TestExampleB/UnitTestProject/WarriorUnitTest.cs
@@ -11,6 +11,22 @@ namespace UnitTestProject
         }
 
         [TestMethod]
+        public void TestWarrior_Constructor_NameWhiteSpace_Exception()
+        {
+            // Arrange, Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => { Warrior testSubject = new Warrior("   ", 100); });
+        }
+
+        [TestMethod]
+        public void TestWarrior_Constructor_NameIsNull_Exception()
+        {
+            string? nullString = null;
+            // Arrange, Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => { Warrior testSubject = new Warrior(nullString, 100); });
+        }
+
+
+        [TestMethod]
         public void TestWarrior_Constructor_HitPointsMinimal_NoException()
         {
             // Act
@@ -20,6 +36,14 @@ namespace UnitTestProject
             Assert.IsNotNull(actualResult);
         }
 
+        [TestMethod]
+        public void TestWarrior_Constructor_HitpointsNegative_Exception()
+        {
+            // Arrange, Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => { Warrior testSubject = new Warrior("A", -1); });
+        }
+
+        // why call it GetOnce? is there a difference if it's several gets?
         [TestMethod]
         public void TestWarrior_Name_GetOnce()
         {
@@ -35,6 +59,20 @@ namespace UnitTestProject
         }
 
         [TestMethod]
+        public void TestWarrior_HitPoints_GetOnce()
+        {
+            // Arrange
+            Warrior testSubject = new Warrior("Rolf", 100);
+            int expectedResult = 100;
+
+            // Act
+            int actualResult = testSubject.HitPoints;
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
         public void TestWarrior_IsDead_GetInitial()
         {
             // Arrange
@@ -43,6 +81,48 @@ namespace UnitTestProject
 
             // Act
             bool actualResult = testSubject.IsDead;
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        // Is this rather a RecieveDamage test?
+        [TestMethod]
+        public void TestWarrior_IsDead_True()
+        {
+            // Arrange
+            Warrior testSubject = new Warrior("Rolf", 100);
+            testSubject.ReceiveDamage(100);
+            bool expectedResult = true;
+
+            // Act
+            bool actualResult = testSubject.IsDead;
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        public void TestWarrior_ReceiveDamage_DamageNegative_Exception()
+        {
+            // Arrange
+            Warrior testSubject = new Warrior("Rolf", 100);
+            
+            // Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => { testSubject.ReceiveDamage(-1); });
+        }
+
+        [TestMethod]
+        public void TestWarrior_ReceiveDamage_30_70()
+        {
+            // Arrange
+            Warrior testSubject = new Warrior("Rolf", 100);
+            testSubject.ReceiveDamage(30);
+            int expectedResult = 70;
+
+            // Act
+            int actualResult = testSubject.HitPoints;
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
