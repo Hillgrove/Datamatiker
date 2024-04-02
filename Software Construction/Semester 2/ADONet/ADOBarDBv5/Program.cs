30567a1ffaea730ceb61ac17c36eb672058eaf25
Jesper Højlund <github@hillgrove.dk>
2024-04-02 12:27:42 +0200

    Added ADONet exercises

---
+++ Software Construction/Semester 2/ADONet/ADOBarDBv5/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// 1) Setup DB
+Setup setup = new Setup("(localdb)\\MSSQLLocalDB", "DrinkDB");
+
+
+// 2a) Read all Ingredients from the DB, and print them (should print 14 Ingredients)
+
+// 2b) Read all Cocktails from the DB, and print them (should print 4 Cocktails)
+
+// 3) Add two new Cocktails
+
+// 4) Read all Cocktails from the DB again, and print them (should print 6 Cocktails)
+
+// 5) Delete the two Cocktails just created.
+
+// 6) Read all Cocktails from the DB again, and print them (should print 6 Cocktails)
---
+++ Software Construction/Semester 2/ADONet/ADOBarDBv5/Services/CocktailDataService.cs
@@ -0,0 +1,80 @@
+﻿
+public class CocktailDataService : IDataService<Cocktail>
+{
+    private IRepository<Ingredient> _ingRepo;
+    private IRepository<CocktailDesolved> _cdRepo;
+    private IRepository<CocktailIngredientDesolved> _cidRepo;
+
+    public CocktailDataService(
+        IRepository<Ingredient> ingRepo,
+        IRepository<CocktailDesolved> cdRepo,
+        IRepository<CocktailIngredientDesolved> cidRepo)
+    {
+        _ingRepo = ingRepo;
+        _cdRepo = cdRepo;
+        _cidRepo = cidRepo;
+    }
+
+    #region Implementation of IDataService interface
+    
+    public int Create(Cocktail c)
+    {
+        // 1) Create cocktail ítself
+        int newId = _cdRepo.Create(c.Desolve());
+        c.Id = newId;
+
+        // 2) Create all ingredients in this specific cocktail
+        c.Ingredients.ForEach(ci => { ci.Id = _cidRepo.Create(ci.Desolve()); });
+
+        return newId;
+    }
+
+    public Cocktail? Read(int id)
+    {
+        return Resolve(_cdRepo.Read(id));
+    }
+
+    public bool Delete(int id)
+    {
+        // 1) Delete cocktail ítself
+        bool deleted = _cdRepo.Delete(id);
+
+        if (!deleted)
+            return false;
+
+        // 2) Delete all cocktail-ingredients associated with the deleted cocktail.
+        GetAllIngredientsForCocktail(id)
+            .ForEachOnEnumerable(ci => _cidRepo.Delete(ci.Id));
+
+        return true;
+    }
+
+    public List<Cocktail> GetAll()
+    {
+        return _cdRepo.GetAll()
+            .Select(cd => Resolve(cd))
+            .ToNullFreeList();
+    }
+
+    #endregion
+
+    private Cocktail? Resolve(CocktailDesolved? cd)
+    {
+        if (cd is null)
+            return null;
+
+        Cocktail c = new Cocktail(cd.Id, cd.Name);
+
+        GetAllIngredientsForCocktail(c.Id)
+            .Select(cid => new { Ingredient = _ingRepo.Read(cid.IngredientId), cid.AmountInCl })
+            .ForEachOnEnumerable(ingAm => c.AddIngredient(ingAm.Ingredient, ingAm.AmountInCl));
+
+        return c;
+    }
+
+    private IEnumerable<CocktailIngredientDesolved> GetAllIngredientsForCocktail(int cocktailId)
+    {
+        return _cidRepo.GetAll()
+            .Where(cid => cid.CocktailId == cocktailId);
+    }
+}
