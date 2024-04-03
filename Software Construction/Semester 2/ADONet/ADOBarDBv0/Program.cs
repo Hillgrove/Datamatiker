877de774a15578bde3c2d365c9d5f65b3d8f25cf
Jesper Højlund <github@hillgrove.dk>
2024-04-03 23:29:41 +0200

    Completed ADONET.0

--- Software Construction/Semester 2/ADONet/ADOBarDBv0/Program.cs
+++ Software Construction/Semester 2/ADONet/ADOBarDBv0/Program.cs
@@ -10,73 +10,161 @@ builder.InitialCatalog = "DrinkDB";
 List<Drink> drinks = new List<Drink>();
 List<Customer> customers = new List<Customer>();
 
+void UpdateDrinks()
+{
+    drinks.Clear();
+
+    try
+    {
+        // 2a) Setup the connection with the "using" syntax
+        using SqlConnection connection = new SqlConnection(builder.ConnectionString);
+        connection.Open();
+
+        // 2b) Prepare and execute the actual SQL command
+        SqlCommand cmd = new SqlCommand("SELECT * FROM DrinkFlat", connection);
+        SqlDataReader reader = cmd.ExecuteReader();
+
+        // 2c) Process the retrieved data
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(0);
+            string name = reader.GetString(1);
+            string alcoholicPart = reader.GetString(2);
+            int alcoholicPartAmount = reader.GetInt32(3);
+            string nonAlcoholicPart = reader.GetString(4);
+            int nonAlcoholicPartAmount = reader.GetInt32(5);
+
+            drinks.Add(new Drink(id, name,
+                alcoholicPart, alcoholicPartAmount,
+                nonAlcoholicPart, nonAlcoholicPartAmount));
+        }
+    }
+    catch (SqlException sqlEx)
+    {
+        Console.WriteLine($"NB: An SqlException occurred : {sqlEx.Message}");
+    }
+}
+
+void UpdateCustomers()
+{
+    customers.Clear();
+
+    try
+    {
+        using SqlConnection connection = new SqlConnection(builder.ConnectionString);
+        connection.Open();
+
+        SqlCommand cmd = new SqlCommand("SELECT * FROM Customer", connection);
+        SqlDataReader reader = cmd.ExecuteReader();
+
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(0);
+            string name = reader.GetString(1);
+            string phone_number = reader.GetString(2);
+            int credit = reader.GetInt32(3);
+
+            customers.Add(new Customer(id, name, phone_number, credit));
+        }
+    }
+    catch (SqlException sqlEx)
+    {
+        Console.WriteLine($"NB: An SqlException occurred : {sqlEx.Message}");
+    }
+}
+
+void DeleteCustomers()
+{
+    try
+    {
+        using SqlConnection connection = new SqlConnection(builder.ConnectionString);
+        connection.Open();
+
+        SqlCommand cmd = new SqlCommand("DELETE FROM Customer " +
+                                        "WHERE Name LIKE 'J%'", connection);
+        cmd.ExecuteNonQuery();
+    }
+    catch (SqlException sqlEx)
+    {
+        Console.WriteLine($"NB: An SqlException occurred : {sqlEx.Message}");
+    }
+}
+
+void PrintDrinks()
+{
+    Console.WriteLine($"All Drinks ({drinks.Count} drinks in total)");
+    Console.WriteLine("-------------------------------------------");
+    foreach (Drink drink in drinks)
+    {
+        Console.WriteLine(drink);
+    }
+}
+
+void PrintCustomers()
+{
+    Console.WriteLine($"All Customers ({customers.Count} customers in total)");
+    Console.WriteLine("-------------------------------------------");
+    foreach (Customer customer in customers)
+    {
+        Console.WriteLine(customer);
+    }
+}
+
+void ResetCustomers()
+{
+    try
+    {
+        string script = File.ReadAllText(@"E:\- Repos -\Datamatiker\Software Construction\" +
+                                         @"Semester 2\ADONet\ADOBarDBv0\ResetCustomer.sql");
+
+        using SqlConnection connection = new SqlConnection(builder.ConnectionString);
+        connection.Open();
+
+        SqlCommand cmd = new SqlCommand(script, connection);
+        cmd.ExecuteNonQuery();
+    }
+    catch (SqlException sqlEx)
+    {
+        Console.WriteLine($"NB: An SqlException occurred : {sqlEx.Message}");
+    }
+}
+
+// initial populating lists
+ResetCustomers();
+UpdateDrinks();
+UpdateCustomers();
+
+// priting lists
+PrintDrinks();
+Console.WriteLine("\n");
+PrintCustomers();
+
+// deleting customers from table
+DeleteCustomers();
+
+// updating list
+UpdateCustomers();
+
+// printing updated list
+Console.WriteLine("\n");
+PrintCustomers();
+
+// Writing to customer table
 try
 {
-    // 2a) Setup the connection with the "using" syntax
     using SqlConnection connection = new SqlConnection(builder.ConnectionString);
     connection.Open();
 
-    // 2b) Prepare and execute the actual SQL command
-    SqlCommand cmd = new SqlCommand("SELECT * FROM DrinkFlat", connection);
-    SqlDataReader reader = cmd.ExecuteReader();
-
-    // 2c) Process the retrieved data
-    while (reader.Read())
-    {
-        int id = reader.GetInt32(0);
-        string name = reader.GetString(1);
-        string alcoholicPart = reader.GetString(2);
-        int alcoholicPartAmount = reader.GetInt32(3);
-        string nonAlcoholicPart = reader.GetString(4);
-        int nonAlcoholicPartAmount = reader.GetInt32(5);
-
-        drinks.Add(new Drink(id, name,
-            alcoholicPart, alcoholicPartAmount,
-            nonAlcoholicPart, nonAlcoholicPartAmount));
-    }
+    SqlCommand cmd = new SqlCommand("INSERT INTO Customer " +
+                                    "VALUES (13,'Anders And', '987-654-3210', '-200')", connection);
+    cmd.ExecuteNonQuery();
 }
+
 catch (SqlException sqlEx)
 {
     Console.WriteLine($"NB: An SqlException occurred : {sqlEx.Message}");
 }
 
-try
-{
-    using SqlConnection connection = new SqlConnection(builder.ConnectionString);
-    connection.Open();
-
-    SqlCommand cmd = new SqlCommand("SELECT * FROM Customer", connection);
-    SqlDataReader reader = cmd.ExecuteReader();
-
-    while (reader.Read())
-    {
-        int id = reader.GetInt32(0);
-        string name = reader.GetString(1);
-        string phone_number = reader.GetString(2);
-        int credit = reader.GetInt32(3);
-
-        customers.Add(new Customer(id, name, phone_number, credit));
-    }
-}
-catch (SqlException sqlEx)
-{
-    Console.WriteLine($"NB: An SqlException occurred : {sqlEx.Message}");
-}
-
-// 3) Print all drinks (should print 10 drinks)
-Console.WriteLine($"All Drinks ({drinks.Count} drinks in total)");
-Console.WriteLine("-------------------------------------------");
-foreach (Drink drink in drinks)
-{
-    Console.WriteLine(drink);
-}
-
-Console.WriteLine();
-Console.WriteLine();
-
-Console.WriteLine($"All Customers ({customers.Count} drinks in total)");
-Console.WriteLine("-------------------------------------------");
-foreach (Customer customer in customers)
-{
-    Console.WriteLine(customer);
-}
+UpdateCustomers();
+Console.WriteLine("\n");
+PrintCustomers();
