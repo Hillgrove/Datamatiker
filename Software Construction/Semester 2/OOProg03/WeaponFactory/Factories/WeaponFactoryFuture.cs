f05e3c09edea0b30a39ab330fb9e8eb3f3994464
Jesper Højlund <github@hillgrove.dk>
2024-04-04 09:15:15 +0200

    Changes to NaiveRPG

---
+++ Software Construction/Semester 2/OOProg03/WeaponFactory/Factories/WeaponFactoryFuture.cs
@@ -0,0 +1,27 @@
+﻿
+/// <summary>
+/// Weapon factory class for futuristic weapons.
+/// </summary>
+public class WeaponFactoryFuture : IWeaponFactory
+{
+    public IWeapon Create(WeaponType type)
+    {
+        // You can do better than that...
+        if (type == WeaponType.Magic)
+        {
+            return new VacuumEnergyChanneler();
+        }
+
+        if (type == WeaponType.Ranged) 
+        {
+            return new Phaser();
+        }
+
+        if (type == WeaponType.Melee)
+        {
+            return new TazerKnuckles();
+        }
+
+        throw new ArgumentException();
+    }
+}
