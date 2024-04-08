7668a113da61a5663288e5f0d20cc603c52d4f31
Jesper Højlund <github@hillgrove.dk>
2024-04-08 10:52:45 +0200

    Added EFCore assignments

---
+++ Software Construction/Semester 2/EFCore/EFCBarDBv4/ModelsCustom/Cocktail.cs
@@ -0,0 +1,25 @@
+﻿
+public partial class Cocktail : IHasId
+{
+    public int Price =>
+        CocktailIngredients
+            .Select(ing => ing.Ingredient.PricePerCl * ing.AmountInCl)
+            .Sum();
+
+    public double AlcoPercent =>
+        CocktailIngredients
+            .Select(ing => ing.Ingredient.AlcoholPercent * ing.AmountInCl)
+            .Sum() / TotalVolume;
+
+    public int TotalVolume =>
+        CocktailIngredients
+            .Select(ing => ing.AmountInCl)
+            .Sum();
+
+    public override string ToString()
+    {
+        string res = $"[{Id}]  {Name}   ({Price} kr., contains {TotalVolume} cl. of {AlcoPercent:F02} %)\n";
+        foreach (CocktailIngredient ing in CocktailIngredients) { res += $"  {ing}\n"; }
+        return res;
+    }
+}
---
+++ Software Construction/Semester 2/EFCore/EFCBarDBv4/ModelsCustom/CocktailIngredient.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Xml.Linq;
+
+public partial class CocktailIngredient : IHasId
+{
+    public override string ToString()
+    {
+        return $"{AmountInCl} cl. of {Ingredient.Name}";
+    }
+}
