b2a31d9b187ff5cab71b3f289b4e679be5385d57
Jesper Højlund <github@hillgrove.dk>
2024-04-08 12:19:29 +0200

    Solved EFCBarDBv1

---
+++ Software Construction/Semester 2/EFCore/EFCBarDBv1/DrinkFlatEx.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace EFCBarDBv1
+{
+    public partial class DrinkFlat
+    {
+        public override string ToString()
+        {
+            return $"{Id}: {Name}. Alcohol: {AlcoholicPart} - {AlcoholicPartAmount} cl. " +
+                $"Non-Alcohol: {NonAlcoholicPart} - {NonAlcoholicPartAmount} cl";
+        }
+    }
+}
