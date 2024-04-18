def3660473de9d0189e0b163e988ba9dec64f5b4
Jesper Højlund <github@hillgrove.dk>
2024-04-18 23:09:52 +0200

    small change

--- Software Construction/Semester 1/Prog02/RolePlayV20/Program.cs
+++ Software Construction/Semester 1/Prog02/RolePlayV20/Program.cs
@@ -2,7 +2,7 @@
 NumberGenerator theNumberGenerator = new NumberGenerator();
 BattleLog theLog = new BattleLog();
 
-Hero theHero = new Hero(5, 30, theNumberGenerator, theLog);
+Hero theHero = new Hero(10, 30, 75, theNumberGenerator, theLog);
 Beast theBeast = new Beast(theNumberGenerator, theLog);
 
 // Now battle...How do we do that (Hint: You need a loop)
