fa926355f6c0c03c36f5893fd4506dc558605dde
Jesper Højlund <github@hillgrove.dk>
2024-08-28 22:56:20 +0200

    Added SchoolLib project

---
+++ Programmering/SchoolLibValidatorUnitTest/SchoolLibValidatorUnitTest/Teacher.cs
@@ -0,0 +1,45 @@
+﻿
+namespace SchoolLibValidatorUnitTest
+{
+    public class Teacher
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Salary { get; private set; }
+
+        public Teacher(int id, string name, int salary) 
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void ValidateName()
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                throw new ArgumentNullException("Name cannot be null or empty.", nameof(Name));
+            }
+        }
+
+        public void ValidateSalary()
+        {
+            if (Salary <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Salary), Salary, "Salary must be a postive value");
+            }
+        }
+
+        public void Validate()
+        {
+            ValidateName();
+            ValidateSalary();
+        }
+        
+        public override string ToString()
+        {
+            return $"{Id}: {Name} - Salary: {Salary}";
+        }
+
+    }
+}
