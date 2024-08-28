52797c49b031154841cbbd194d5005e8117c8437
Jesper Højlund <github@hillgrove.dk>
2024-08-28 23:47:30 +0200

    removed tests

--- Programmering/SchoolLibValidatorUnitTest/SchoolLibValidatorUnitTest/Teacher.cs
+++ Programmering/SchoolLibValidatorUnitTest/SchoolLibValidatorUnitTest/Teacher.cs
@@ -40,6 +40,5 @@ namespace SchoolLibValidatorUnitTest
         {
             return $"{Id}: {Name} - Salary: {Salary}";
         }
-
     }
 }
---
+++ Programmering/SchoolLibValidatorUnitTest/SchoolLibValidatorUnitTestTests/TeacherTests.cs
@@ -0,0 +1,16 @@
+
+using SchoolLibValidatorUnitTest;
+
+namespace SchoolLibValidatorUnitTest.Tests
+{
+    [TestClass]
+    public class TeacherTests
+    {
+        private Teacher 
+        
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
