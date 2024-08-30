6c904fa54930f093f8286e546c10c1f6fa5259e8
Jesper Højlund <github@hillgrove.dk>
2024-08-30 14:29:48 +0200

    added JSON Exercise 1

---
+++ Teknologi/JSON Exercise 1/Models/Car.cs
@@ -0,0 +1,32 @@
+﻿
+namespace JSON_Exercise_1.Models
+{
+    internal class Car
+    {
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public string? Color { get; set; }
+        public int Mileage { get; set; }
+
+        public Car()
+        {
+        }
+        
+        
+        public Car(string brand, string model, string color, int mileage)
+        {
+            Brand = brand;
+            Model = model;
+            Color = color;
+            Mileage = mileage;
+        }
+
+        public override string ToString()
+        {
+            //“Brand: BMW - Model: 330e - Color: Green - Mileage: 45721”
+            return $"Brand: {Brand} - Model: {Model} - Color: {Color} - Mileage: {Mileage}";
+        }
+
+
+    }
+}
---
+++ Teknologi/JSON Exercise 1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using JSON_Exercise_1.Models;
+using System.Text.Json;
+
+
+// Part 1
+Car carA = new Car("Opel", "Kadet", "Red", 10000);
+Console.WriteLine(carA);
+
+// Part 2
+string expectedCarSerialization = """{"Brand":"Opel","Model":"Kadet","Color":"Red","Mileage":10000}""";
+string serializedCar = JsonSerializer.Serialize(carA);
+
+Console.WriteLine(expectedCarSerialization);
+Console.WriteLine(serializedCar);
+
+Car carB = new Car();
+List<Car> carList = new List<Car>  { carA, carB };
+
+string expectectListSerialization = """[{"Brand":"Opel","Model":"Kadet","Color":"Red","Mileage":10000},{"Brand":null,"Model":null,"Color":null,"Mileage":0}]""";
+string serializedList =  JsonSerializer.Serialize(carList);
+
+Console.WriteLine(expectectListSerialization);
+Console.WriteLine(serializedList);
+
+// Part 3
