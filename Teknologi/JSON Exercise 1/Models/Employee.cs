f9143129b9e55753b7ce5f3c08078bd90db0dcfc
Jesper Højlund <github@hillgrove.dk>
2024-09-01 23:34:06 +0200

    Solved JSON exercise 1

---
+++ Teknologi/JSON Exercise 1/Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Dynamic;
+using System.Net;
+using System.Reflection;
+
+namespace JSON_Exercise_1.Models
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int MonthsEmployed { get; set; }
+        public List<string>? JobAreas { get; set; }
+
+        public Employee(string name, int salary, int monthsEmployed, List<string>? jobAreas)
+        {
+            Name = name;
+            Salary = salary;
+            MonthsEmployed = monthsEmployed;
+            JobAreas = jobAreas;
+        }
+
+        public override string ToString()
+        {
+            //return $"Dealership Name: {Name}\n" +
+            //       $"Address: {Address}\n" +
+            //       $"Cars:\n" +
+            //       $"{carsInfo}\n" +
+            //       $"Employees:\n" +
+            //       $"{employeesInfo}";
+
+            var jobInfo = JobAreas != null && JobAreas.Count > 0
+                ? string.Join(", ", JobAreas.Select(job => job.ToString()))
+                : "No jobinfo available";
+            return $"Name: {Name} - Salary: {Salary} - MonthsEmployed: {MonthsEmployed} - JobAreas: {jobInfo}";
+        }
+    }
+}
