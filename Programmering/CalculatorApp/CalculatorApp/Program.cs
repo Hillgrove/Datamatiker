cc865afbe6eb609b8f6e74e086129b9957597905
Jesper Højlund <github@hillgrove.dk>
2024-09-07 12:47:50 +0200

    Soved Calculator app

--- Programmering/CalculatorApp/CalculatorApp/Program.cs
+++ Programmering/CalculatorApp/CalculatorApp/Program.cs
@@ -1,27 +1,33 @@
 ﻿
 using SharedCalculator;
 
-List<string> VALID_OPERATORS = new List<string>() { "+", "-", "*", "/" };
+List<string> VALID_OPERATORS = new List<string>() { "+", "-", "*", "/", "quit" };
 Calculator calculator = new Calculator();
 double x;
 double y;
 double result;
 
-Console.WriteLine("Welcome to the C.A.L.C.U.L.A.T.O.R");
+Console.WriteLine("Welcome to the C.A.L.C.U.L.A.T.O.R.");
+Console.WriteLine("To quit type \"quit\"");
 
 bool isRunning = true;
 do
 {
-    Console.Write("Which calculation do you want to do (+, -, *, /): ");
-    string? @operator = Console.ReadLine();
+    Console.Write("\nWhich calculation do you want to do (+, -, *, /): ");
+    string? @operator = Console.ReadLine()?.ToLower();
 
     if (!string.IsNullOrEmpty(@operator) && VALID_OPERATORS.Contains(@operator))
     {
+        if (@operator == "quit")
+        {
+            isRunning = false;
+            break;
+        }
+
         Console.Write("Value of x: ");
         x = double.Parse(Console.ReadLine()!);
         Console.Write("Value of y: ");
         y = double.Parse(Console.ReadLine()!);
-
 
         switch (@operator)
         {
@@ -36,18 +42,17 @@ do
                 break;
 
             case "*":
-                // TODO
+                result = calculator.Multiply(x, y);
+                Console.WriteLine($"{x} {@operator} {y} = {result}");
                 break;
 
             case "/":
-                // TODO
-                break;
-
-            default:
-                // TODO
+                result = calculator.Divide(x, y);
+                Console.WriteLine($"{x} {@operator} {y} = {result}");
                 break;
         }
     }
-
 }
 while (isRunning);
+
+Console.WriteLine("\nThanks for using C.A.L.C.U.L.A.T.O.R.");
