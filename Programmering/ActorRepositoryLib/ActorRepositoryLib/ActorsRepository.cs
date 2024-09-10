8c63b5e0c00552a22773cfaa2d0e44a1768ec570
Jesper Højlund <github@hillgrove.dk>
2024-09-10 14:38:31 +0200

    Added Actor repo

---
+++ Programmering/ActorRepositoryLib/ActorRepositoryLib/ActorsRepository.cs
@@ -0,0 +1,56 @@
+﻿
+namespace ActorRepositoryLib
+{
+    public class ActorsRepository
+    {
+        private int _nextId = 1;
+        private List<Actor> _actors = new();
+
+        public Actor Add(Actor actor)
+        {
+            actor.Validate();
+            actor.Id = _nextId++;
+            _actors.Add(actor);
+            return actor;
+        }
+
+        public Actor? Get(int id)
+        {
+            return _actors.Find(actor => actor.Id == id);
+        }
+
+        public IEnumerable<Actor> GetAll()
+        {
+            IEnumerable<Actor> actors = new List<Actor>(_actors);
+            return actors;
+        }
+
+        public Actor? Update(int id, Actor actor)
+        {
+            actor.Validate();
+            Actor? existingActor = Get(id);
+
+            if (existingActor == null)
+            {
+                return null;
+            }
+
+            existingActor.Name = actor.Name;
+            existingActor.BirthYear = actor.BirthYear;
+            return existingActor;
+        }
+
+        public Actor? Delete(int id)
+        {
+            Actor? actor = Get(id);
+
+            if (actor == null)
+            {
+                return null;
+            }
+
+            _actors.Remove(actor);
+            return actor;
+        }
+    }
+}
---
+++ Programmering/ActorRepositoryLib/ActorRepositoryLibTests/ActorsRepositoryTests.cs
@@ -0,0 +1,37 @@
+﻿
+namespace ActorRepositoryLib.Tests
+{
+    [TestClass()]
+    public class ActorsRepositoryTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
