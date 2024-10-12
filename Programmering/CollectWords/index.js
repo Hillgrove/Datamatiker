fbdaba4a2437f58fb0873f923598f6f6a9520b9b
Jesper Højlund <github@hillgrove.dk>
2024-10-12 15:27:48 +0200

    fix: corrected Save logic

--- Programmering/CollectWords/index.js
+++ Programmering/CollectWords/index.js
@@ -1,23 +1,27 @@
 const app = Vue.createApp({
     data() {
         return {
-            
-            Word: String,
-            Words: [],
-            Message: "testing",
-            ShowMessage: false
+            word: null,
+            words: [],
+            message: null
         }
     },
 
     methods: {
         Save(word) {
-            this.Words.push(word)
+            this.words.push(word)
         },
         Clear() {
-            this.Words = []
+            this.words = []
+            this.message = null
         },
         Show() {
-            this.ShowMessage = true
+            if (this.words == null || this.words.length == 0) {
+                this.message = "empty"
+            }
+            else {
+                this.message = this.words.toString()
+            }
         }
     }
 })
