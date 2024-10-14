d2c20955c0e969237e8cd23d0fcba924e91343ea
Jesper Højlund <github@hillgrove.dk>
2024-10-14 17:37:43 +0200

    feat: add initial behaviour

---
+++ Teknologi/JSON Exercise 1/Models/Car.cs
@@ -0,0 +1,34 @@
+﻿
+namespace JSON_Exercise_1.Models
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string  Model { get; set; }
+        public string Color { get; set; }
+        public int Mileage { get; set; }
+
+        public Car()
+        {
+            Brand = "Undefined";
+            Model = "Undefined";
+            Color = "Undefined";
+            Mileage = -1;
+        }
+
+        public Car(string? brand = null, string? model = null, string? color = null, int mileage = -1)
+        {
+            Brand = string.IsNullOrWhiteSpace(brand) ? "Undefined" : brand.Trim();
+            Model = string.IsNullOrWhiteSpace(model) ? "Undefined" : model.Trim();
+            Color = string.IsNullOrWhiteSpace(color) ? "Undefined" : color.Trim();
+
+            Mileage = mileage;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Brand: {Brand} - Model: {Model} - Color: {Color} - Mileage: {Mileage}";
+        }
+    }
+}
--- Teknologi/JSON Exercise 1/Program.cs
+++ Teknologi/JSON Exercise 1/Program.cs
@@ -1,1 +1,26 @@
 ﻿
+using JSON_Exercise_1.Models;
+
+Car carSis = new Car(brand: "Peugeot", model: "206", color: "Black");
+Console.WriteLine(carSis);
+
+string? brand;
+string? model;
+string? color;
+int mileage;
+
+Console.Write("\nType car brand: ");
+brand = Console.ReadLine();
+Console.Write("Type car model: ");
+model = Console.ReadLine();
+Console.Write("Type car color: ");
+color = Console.ReadLine();
+Console.Write("Type car mileage: ");
+if (!int.TryParse(Console.ReadLine(), out mileage))
+{
+    mileage = -1;
+}
+
+Car newCar = new Car(brand, model, color, mileage);
+Console.WriteLine("\nThis is your new car:");
+Console.WriteLine(newCar);
