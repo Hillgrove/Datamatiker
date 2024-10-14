72a72295629cf44bce545f2c197911119f9c4d47
Jesper Højlund <github@hillgrove.dk>
2024-10-14 18:20:25 +0200

    feat: add JSON serialization

--- Teknologi/JSON Exercise 1/Program.cs
+++ Teknologi/JSON Exercise 1/Program.cs
@@ -1,15 +1,13 @@
 ﻿
 using JSON_Exercise_1.Models;
-
-Car carSis = new Car(brand: "Peugeot", model: "206", color: "Black");
-Console.WriteLine(carSis);
+using System.Text.Json;
 
 string? brand;
 string? model;
 string? color;
 int mileage;
 
-Console.Write("\nType car brand: ");
+Console.Write("Type car brand: ");
 brand = Console.ReadLine();
 Console.Write("Type car model: ");
 model = Console.ReadLine();
@@ -18,9 +16,22 @@ color = Console.ReadLine();
 Console.Write("Type car mileage: ");
 if (!int.TryParse(Console.ReadLine(), out mileage))
 {
-    mileage = -1;
+    if (mileage < 0)
+    {
+        mileage = -1;
+    }
 }
 
 Car newCar = new Car(brand, model, color, mileage);
 Console.WriteLine("\nThis is your new car:");
 Console.WriteLine(newCar);
+
+Console.WriteLine();
+
+string jsonExpected = $$"""{"Brand":"{{newCar.Brand}}","Model":"{{newCar.Model}}","Color":"{{newCar.Color}}","Mileage":{{newCar.Mileage}}}""";
+string jsonCar = JsonSerializer.Serialize(newCar);
+
+Console.Write(jsonExpected);
+Console.WriteLine(" - Expected JSON string");
+Console.Write(jsonCar);
+Console.WriteLine(" - Actual JSON string");
