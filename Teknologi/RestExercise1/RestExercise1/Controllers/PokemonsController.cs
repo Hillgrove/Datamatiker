279eefd1fb0d3206a17377aaf605217ee90dd262
Jesper Højlund <github@hillgrove.dk>
2024-10-17 15:50:07 +0200

    feat: add singleton

--- Teknologi/RestExercise1/RestExercise1/Controllers/PokemonsController.cs
+++ Teknologi/RestExercise1/RestExercise1/Controllers/PokemonsController.cs
@@ -1,41 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PokemonLib;
 
 namespace RestExercise1.Controllers
 {
     [Route("api/[controller]")]
+    // the controller is available on ..../api/pokemons
+    // [controller] means the name of the controller minus "Controller"
     [ApiController]
     public class PokemonsController : ControllerBase
     {
+        private readonly PokemonsRepository _repository;
+
+        public PokemonsController(PokemonsRepository repository)
+        {
+            _repository = repository;
+        }
+
         // GET: api/<PokemonsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Pokemon> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _repository.GetAll();
         }
 
         // GET api/<PokemonsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Pokemon Get(int id)
         {
-            return "value";
+            return _repository.GetByID(id);
         }
 
         // POST api/<PokemonsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public Pokemon Post([FromBody] Pokemon value)
         {
+            return _repository.Add(value);
         }
 
         // PUT api/<PokemonsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public Pokemon Put(int id, [FromBody] Pokemon value)
         {
+            return _repository.Update(id, value);
         }
 
         // DELETE api/<PokemonsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public Pokemon Delete(int id)
         {
+            return _repository.Delete(id);
         }
     }
 }
