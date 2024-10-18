97c8011629c66abfc033ea2499b73452c437bfba
Jesper Højlund <github@hillgrove.dk>
2024-10-18 14:33:55 +0200

    feat: add swagger

--- Teknologi/RestExercise1/RestExercise1/Program.cs
+++ Teknologi/RestExercise1/RestExercise1/Program.cs
@@ -1,15 +1,22 @@
 using PokemonLib;
+using YamlDotNet.Serialization.ObjectGraphVisitors;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddOpenApiDocument();
 builder.Services.AddSingleton<PokemonsRepository>(new PokemonsRepository());
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi();
+}
 
 app.UseAuthorization();
 
