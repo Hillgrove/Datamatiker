43fdd32d4f35670d7fdbadefa2507b7c351eb0eb
Jesper Højlund <github@hillgrove.dk>
2024-10-24 15:19:59 +0200

    feat: add default controller

---
+++ Teknologi/REST Exercises/REST Exercise 1/Controllers/PokemonsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace REST__Exercise_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonsController : ControllerBase
+    {
+        // GET: api/<PokemonsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PokemonsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PokemonsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PokemonsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PokemonsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
