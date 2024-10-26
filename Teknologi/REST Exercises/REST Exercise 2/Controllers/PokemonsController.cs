42647d0babf68c98b758c2bc011544fbd36f91cf
Jesper Højlund <github@hillgrove.dk>
2024-10-26 18:37:31 +0200

    feat: solve exercise 2

--- Teknologi/REST Exercises/REST Exercise 2/Controllers/PokemonsController.cs
+++ Teknologi/REST Exercises/REST Exercise 2/Controllers/PokemonsController.cs
@@ -6,7 +6,7 @@ namespace REST_Exercise_2.Controllers
 {
 
     [Route("api/[controller]")]
-    // the controller is available on ..../api/books
+    // the controller is available on ..../api/pokemons
     // [controller] means the name of the controller minus "Controller"
     [ApiController]
     public class PokemonsController : ControllerBase
@@ -18,39 +18,129 @@ namespace REST_Exercise_2.Controllers
             _repository = repository;
         }
 
-        // GET: api/<BooksController>
+        // GET: api/<PokemonsController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [HttpGet]
-        public IEnumerable<Pokemon> Get()
+        public ActionResult<IEnumerable<Pokemon>> Get()
         {
-            return _repository.GetAll();
+            IEnumerable<Pokemon> pokemons = _repository.GetAll();
+
+            if (!pokemons.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(pokemons);
         }
 
-        // GET api/<BooksController>/5
+        // GET api/<PokemonsController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("{id}")]
-        public Pokemon Get(int id)
+        public ActionResult<Pokemon> Get(int id)
         {
-            return _repository.GetByID(id);
+            Pokemon? pokemon = _repository.GetByID(id);
+
+            if (pokemon == null)
+            {
+                return NotFound("No such class, id: {id}");
+            }
+
+            return Ok(pokemon);
         }
 
-        // POST api/<BooksController>
+        // POST api/<PokemonsController>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPost]
-        public Pokemon Post([FromBody] Pokemon value)
+        public ActionResult<Pokemon> Post([FromBody] Pokemon value)
         {
-            return _repository.Add(value);
+            var createdPokemon = new Pokemon();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                createdPokemon = _repository.Add(value);
+                string? uri = Url.Action(nameof(Get), new { id = createdPokemon.Id });
+
+                if (string.IsNullOrEmpty(uri))
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, "An unexpected error occurred.");
+                }
+
+                return Created(uri, createdPokemon);
+            }
+
+            //if (Name == null) throw new ArgumentNullException();
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest($"Name cannot be null. {ex}");
+            }
+
+            //if (Name.Length <= 2) throw new ArgumentOutOfRangeException("specifik besked");
+            catch (ArgumentOutOfRangeException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-        // PUT api/<BooksController>/5
+        // PUT api/<PokemonsController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpPut("{id}")]
-        public Pokemon Put(int id, [FromBody] Pokemon value)
+        public ActionResult<Pokemon> Put(int id, [FromBody] Pokemon value)
         {
-            return _repository.Update(id, value);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Pokemon? updatedPokemon = _repository.Update(id, value);
+
+                if (updatedPokemon == null)
+                {
+                    return NotFound($"No pokemon found with id: {id}");
+                }
+
+                return Ok(updatedPokemon);
+            }
+
+            //if (Name == null) throw new ArgumentNullException();
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest($"Name cannot be null. {ex}");
+            }
+
+            //if (Name.Length <= 2) throw new ArgumentOutOfRangeException("specifik besked");
+            catch (ArgumentOutOfRangeException ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-        // DELETE api/<BooksController>/5
+        // DELETE api/<PokemonsController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpDelete("{id}")]
-        public Pokemon Delete(int id)
+        public ActionResult<Pokemon> Delete(int id)
         {
-            return _repository.Delete(id);
+            Pokemon? deletedPokemon = _repository.Delete(id);
+
+            if (deletedPokemon == null)
+            {
+                return NotFound($"No pokemon found with id: {id}");
+            }
+
+            return Ok(deletedPokemon);
         }
-    }       
+    }
 }
