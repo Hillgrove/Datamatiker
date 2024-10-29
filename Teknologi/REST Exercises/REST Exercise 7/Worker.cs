d0e0f0b60be1f1d26f62bc2559d1a0c1f9b6f53f
Jesper Højlund <github@hillgrove.dk>
2024-10-29 13:30:20 +0100

    feat: add consumption of paginated GET

--- Teknologi/REST Exercises/REST Exercise 7/Worker.cs
+++ Teknologi/REST Exercises/REST Exercise 7/Worker.cs
@@ -19,6 +19,15 @@ namespace REST_Exercise_7
             }
             Console.WriteLine();
 
+            // GET pagination test
+            Console.WriteLine("GET PAGINATION TEST:");
+            list = await GetPaginatedPokemons("5", "2");
+            foreach (var pokemon in list)
+            {
+                Console.WriteLine(pokemon);
+            }
+            Console.WriteLine();
+
             // POST test
             Console.WriteLine("POST TEST:");
             var newPokemon = new Pokemon { Name = "Test", Level = 13 };
@@ -31,6 +40,25 @@ namespace REST_Exercise_7
             using (var client = new HttpClient())
             {
                 HttpResponseMessage response = await client.GetAsync(URL);
+                IEnumerable<Pokemon> list = await response.Content.ReadFromJsonAsync<IEnumerable<Pokemon>>();
+                return list;
+            }
+        }
+
+        public async Task<IEnumerable<Pokemon>> GetPaginatedPokemons(string startIndex, string amount)
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri(URL + "/paginated")
+                };
+
+                request.Headers.Add("startIndex", startIndex);
+                request.Headers.Add("amount", amount);
+
+                HttpResponseMessage response = await client.SendAsync(request);
                 IEnumerable<Pokemon> list = await response.Content.ReadFromJsonAsync<IEnumerable<Pokemon>>();
                 return list;
             }
