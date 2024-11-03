fb5d32fd57478a02a3bffdf040c43011663a832b
Jesper Højlund <github@hillgrove.dk>
2024-11-03 21:38:01 +0100

    feat: add todo w. vue and axios

---
+++ Programmering/Todos/index.js
@@ -0,0 +1,49 @@
+const baseUri = "https://jsonplaceholder.typicode.com/todos"
+
+Vue.createApp({
+    data() {
+        return {
+            todos: [],
+            error: null,
+            userId: ""
+        }
+    },
+
+    async created() {
+        // created() is a life cycle method, not an ordinary method
+        // created() is called automatically when the page is loaded
+        console.log("created method called")
+        this.getTodos()
+    },
+
+    methods: {
+        async getTodos(userId = null) {
+            const uri = userId ? `${baseUri}?userId=${userId}` : baseUri;
+            try {
+                const response = await axios.get(uri)
+                this.todos = await response.data
+                this.error = null
+            } 
+            catch (ex) {
+                this.todos = []
+                this.error = ex.message
+            }
+        },
+
+        cleanList() {
+            this.todos = []
+            this.error = []
+        },
+
+        async getByUserId(userId) {
+            if (!userId) {
+                this.error = "No user id is provided."
+                this.todos = []
+            }
+            else {
+                console.log("getByUserId: Fetching todos for user " + userId);
+                this.getTodos(userId)
+            }
+        }
+    }
+}).mount("#app")
