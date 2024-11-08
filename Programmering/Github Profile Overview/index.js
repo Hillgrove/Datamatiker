aa8cc83c2d8231925b67568199c7f8552c065092
Jesper Højlund <github@hillgrove.dk>
2024-11-08 21:10:08 +0100

    feat: add ideas and small setup

--- Programmering/Github Profile Overview/index.js
+++ Programmering/Github Profile Overview/index.js
@@ -1,5 +1,26 @@
 const baseUrl = "https://api.github.com/users/hillgrove/repos"
 
-vue.createApp({
+Vue.createApp({
+    data() {
+        return {
+            repos: [],
+            error: null
+        }
+    },
 
+    async created() {
+        this.getAll(baseUrl)
+    },
+
+    methods: {
+        async getAll(url) {
+            try {
+                const response = await axios.get(url)
+                this.repos = response.data
+            }
+            catch (ex) {
+                this.error = ex.message 
+            }
+        }
+    }
 }).mount("#app")
---
+++ Programmering/Test/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';
+import MainApp from './MainApp.js';
+
+createApp(MainApp).mount('#app');
