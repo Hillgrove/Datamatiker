be2549fa6a42f6dc3adfbd52672c9615e8052e93
Jesper Højlund <github@hillgrove.dk>
2024-11-12 17:01:30 +0100

    feat: chore: remake API class

--- Teknologi/Sensor REST Service/Sensor REST Service/Controllers/SensorDataController.cs
+++ Teknologi/Sensor REST Service/Sensor REST Service/Controllers/SensorDataController.cs
@@ -2,7 +2,7 @@
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Sensor_REST_API.Controllers
+namespace Sensor_REST_Service.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
