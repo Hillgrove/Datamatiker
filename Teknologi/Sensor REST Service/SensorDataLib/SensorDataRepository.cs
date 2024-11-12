a225ab1e3c8c99c6562e3971440583937c522172
Jesper Højlund <github@hillgrove.dk>
2024-11-12 16:50:23 +0100

    feat: add SensorData repository

---
+++ Teknologi/Sensor REST Service/SensorDataLib/SensorDataRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace SensorDataLib
+{
+    public class SensorDataRepository
+    {
+        private List<SensorData> _sensorData;
+        private int _nextId;
+
+        public SensorDataRepository()
+        {
+            _sensorData = new List<SensorData>();
+            _nextId = 1;
+        }
+
+        public List<SensorData> GetAll()
+        {
+            return new List<SensorData>(_sensorData);
+
+        }
+
+        public SensorData Add(SensorData sensorData)
+        {
+            sensorData.Id = _nextId++;
+            _sensorData.Add(sensorData);
+            return sensorData;
+        }
+    }
+}
