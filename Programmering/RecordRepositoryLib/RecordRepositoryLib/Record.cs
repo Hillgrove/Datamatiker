d94f9d84e1779f53581fdca992358d6b814a1925
Jesper Højlund <github@hillgrove.dk>
2024-11-22 12:51:35 +0100

    feat: add Record model

---
+++ Programmering/RecordRepositoryLib/RecordRepositoryLib/Record.cs
@@ -0,0 +1,11 @@
+﻿namespace RecordRepositoryLib
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public float Duration { get; set; }
+        public int ReleaseYear { get; set; }        
+    }
+}
