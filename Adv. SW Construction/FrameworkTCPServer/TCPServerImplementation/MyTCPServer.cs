61560be41cb5130188513b6299efec067dac041a
Jesper Højlund <github@hillgrove.dk>
2025-02-13 22:29:25 +0100

    feat: Add MyTCPServer class

---
+++ Adv. SW Construction/FrameworkTCPServer/TCPServerImplementation/MyTCPServer.cs
@@ -0,0 +1,23 @@
+﻿using ServerFramework.TCPServer;
+
+namespace TCPServerImplementation
+{
+    public class MyTCPServer : AbstractTCPServer
+    {
+        public MyTCPServer(int port, string serverName) : base(port, serverName)
+        {
+        }
+
+        protected override void TcpServerWork(StreamReader sr, StreamWriter sw)
+        {
+            string? line = sr.ReadLine();
+            if (line == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            int wordCount = line.Split().Length;
+            sw.WriteLine($"Number of words in \"{line}\" is {wordCount}");
+        }
+    }
+}
---
+++ Adv. SW Construction/FrameworkTCPServer/TCPServerImplementation/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using TCPServerImplementation;
+
+var server = new MyTCPServer(7, "My Custom Server");
+server.Start();
