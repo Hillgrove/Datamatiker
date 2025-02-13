9032a54753c4f1a43e3ff443e6be0c5c734a8e57
Jesper Højlund <github@hillgrove.dk>
2025-02-13 22:27:38 +0100

    feat: Add Echo Client project with basic TCP communication

---
+++ Adv. SW Construction/FrameworkTCPServer/EchoClient/Client.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Sockets;
+
+namespace EchoClient
+{
+    public class Client
+    {
+        public void Start()
+        {
+            using TcpClient socket = new TcpClient("localhost", 7);
+            using NetworkStream ns = socket.GetStream();
+            using StreamReader sr = new StreamReader(ns);
+            using StreamWriter sw = new StreamWriter(ns);
+
+            sw.WriteLine("Some random message");
+            sw.Flush();
+
+            string? line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+    }
+}
