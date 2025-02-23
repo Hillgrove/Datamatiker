b5dce630c4518c652c4e627b644a58534de72273
Jesper Højlund <github@hillgrove.dk>
2025-02-23 14:02:05 +0100

    feat: add timing of insertion

--- Adv. Database/Exercise 3 - Data Insert/ImdbDataInsert/IMDBInsertApproaches/Program.cs
+++ Adv. Database/Exercise 3 - Data Insert/ImdbDataInsert/IMDBInsertApproaches/Program.cs
@@ -1,11 +1,15 @@
 ﻿using IMDBInsertApproaches;
 
 string filepath = @"C:\Programming\IMDBData\title.basics.tsv";
-int linesToRead = 500000;
+int linesToRead = 50000;
 
+DateTime startTime = DateTime.Now;
 var titles = TSVReader.ReadFromFile(filepath, linesToRead);
+Console.WriteLine();
 
-for (int i = 0; i < 20; i++)
-{
-    Console.WriteLine(titles[i]);
-}
+NormalInserter.InsertData(titles);
+DateTime endTime = DateTime.Now;
+
+double seconds = endTime.Subtract(startTime).TotalSeconds;
+Console.WriteLine($"Inserting {linesToRead} records using Normal Insert takes {seconds:F2} seconds");
+Console.WriteLine($"Inserting 11.000.000 records would take an estimated time of {11000000 / linesToRead * seconds:F2} seconds");
