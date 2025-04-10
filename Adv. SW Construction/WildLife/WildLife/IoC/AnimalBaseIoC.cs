5b89df35cfbc1ff33cdafa0e6382652bd61fa669
Jesper Højlund <github@hillgrove.dk>
2025-04-10 23:41:58 +0200

    Add NaiveRPG and WildLife projects with initial setup

---
+++ Adv. SW Construction/WildLife/WildLife/IoC/AnimalBaseIoC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using WildLife.Common;
+
+namespace WildLife.IoC
+{
+    public abstract class AnimalBaseIoC : AnimalBase
+    {
+        protected AnimalBaseIoC(AnimalKind kind, AnimalGender gender) : base(kind, gender)
+        {
+        }
+
+        public override void Act()
+        {
+            foreach (var (condition, behavior) in GetPrioritizedBehaviors())
+            {
+                if (condition())
+                {
+                    behavior();
+                    return;
+                }
+            }
+
+            Idle();
+        }
+
+        protected abstract List<(Func<bool> condition, Action behavior)> GetPrioritizedBehaviors();
+    }
+}
---
+++ Adv. SW Construction/WildLife/WildLife/IoC/FoxIoC.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using WildLife.Common;
+
+namespace WildLife.IoC
+{
+    public class FoxIoC : AnimalBaseIoC
+    {
+        public FoxIoC(AnimalGender gender) : base(AnimalKind.fox, gender)
+        {    
+        }
+
+        protected override List<(Func<bool> condition, Action behavior)> GetPrioritizedBehaviors()
+        {
+            return new List<(Func<bool>, Action)>
+            {
+                (
+                    () => Hungry && (TheWorld.NearBy(AnimalKind.rabbit) || TheWorld.NearBy(AnimalKind.mouse)),
+                    () => { Hunt(); Eat(); }
+                ),
+
+                (
+                    () => TheWorld.NearBy(AnimalKind.fox) && TheWorld.GenderOfNearBy(AnimalKind.fox) != Gender,
+                    () => Mate()
+                ),
+
+                (
+                    () => TheWorld.NearBy(AnimalKind.tiger),
+                    () => Flee()
+                ),
+
+                (
+                    () => Sleepy,
+                    () => Sleep()
+                )
+            };
+        }
+    }
+}
